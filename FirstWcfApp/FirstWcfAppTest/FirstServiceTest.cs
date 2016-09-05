8354b184b2a2f1b763ab247a0a77836fed478c97
jdamore <jean.damore@gmail.com>
2016-09-05 22:29:01 +1000

    Added first NUnit unit test and associated PSake task

---
+++ FirstWcfApp/FirstWcfAppTest/FirstServiceTest.cs
@@ -0,0 +1,27 @@
+﻿namespace FirstWcfAppTest
+{
+    using NUnit.Framework;
+    using FirstWcfApp;
+
+    public class FirstServiceTest
+    {
+        private IFirstService service;
+
+        [SetUp]
+        public void SetUp()
+        {
+            service = new FirstService();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void GetReturnsTheExpectedResult()
+        {
+            Assert.AreEqual("You entered ID: test", service.Get("test"));
+        }
+    }
+}
--- FirstWcfApp/go.ps1
+++ FirstWcfApp/go.ps1
@@ -1,18 +1,26 @@
+# Build properties
 properties {
   $message = 'Welcome to FirstWcfApp build!'
 }
 
-$message
 
-task default -depends Test
+# Default task
+task default -depends Clean, Compile, Test
 
-task Test -depends Compile, Clean { 
-  $message
+
+# Clean all DLLs
+task Clean { 
+  Get-ChildItem $PSScriptRoot\FirstWcfApp -recurse -include *.dll -force | Remove-Item -v
+  Get-ChildItem $PSScriptRoot\FirstWcfAppTest -recurse -include *.dll -force | Remove-Item -v
 }
 
-task Compile -depends Clean { 
+# Compile the solution
+task Compile { 
   MSBuild.exe FirstWcfApp.sln /t:rebuild /p:VisualStudioVersion=14.0
 }
 
-task Clean { 
+# Runs the tests
+task Test {
+  $env:Path += ";$PSScriptRoot\packages\NUnit.ConsoleRunner.3.4.1\tools"
+  nunit3-console.exe $PSScriptRoot\FirstWcfAppTest\bin\Debug\FirstWcfAppTest.dll
 }
