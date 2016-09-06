e3f4b154e2beaa36bb36d97c6b14f012f99a348f
jdamore <jean.damore@gmail.com>
2016-09-06 21:50:59 +1000

    Added installation of NuGet packages as part of go.cmd script

--- FirstWcfApp/go.cmd
+++ FirstWcfApp/go.cmd
@@ -1,5 +1,9 @@
 @echo off
 
+@rem install NuGet packages
+.\nuget.exe install FirstWcfApp\packages.config -o .\packages
+.\nuget.exe install FirstWcfAppTest\packages.config -o .\packages
+
 @rem set path to psake utility
 set PATH=%PATH%;%~dp0\packages\psake.4.6.0\tools
 
